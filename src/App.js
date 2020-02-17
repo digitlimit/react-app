710a070b889f4cd4d134478287148d6f71329ec3
Emeka Mbah <emba@ovh-embah.ovhenergy.local>
2020-02-17 19:55:16 +0100

    Add Person

--- src/App.js
+++ src/App.js
@@ -22,19 +22,42 @@ class App extends Component {
       ]})
   };
 
+  onTextChange = (e) => {
+      this.setState({persons: [
+              {name: 'Emeka', age: 47},
+              {name: e.target.value, age: 30},
+              {name: 'Amakanen', age: 21, hobby: 'I love cakes'},
+          ]})
+  };
+
   render() {
 
-    let listPersons = this.state.persons.map(function(person, index){
-      return <Person key={index} name={person.name} age={person.age}/>
+    let listPersons = this.state.persons.map((person, index) => {
+      return <Person changed={this.onTextChange} key={index} name={person.name} age={person.age}/>
     });
+
+    const btnStyle = {
+        backgroundColor: 'green',
+        border: '1px solid #eee',
+        marginTop: '50px',
+        padding: '8px',
+        color: 'white',
+        cursor: 'pointer'
+    }
 
     return (
       <div className="App">
-        Hello Franklin!
-        <button className="add-btn" onClick={this.addNewHandler.bind(this, 'Emeks')}>
+
+        <button  style={btnStyle} className="add-btn" onClick={this.addNewHandler.bind(this, 'Emeks')}>
           Add New
         </button>
-        <Person click={this.addNewHandler.bind(this, 'Emeks')} name="Emekaz" age="200"/>
+
+        {/*<Person*/}
+        {/*  */}
+        {/*    click={this.addNewHandler.bind(this, 'Emeks')}*/}
+        {/*    name="Emekaz"*/}
+        {/*    age="200"/>*/}
+
         {listPersons}
       </div>
     );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './Person.css'
 
 const person = (props) => {
     return (
-        <div className="person">
+        <div className="Person">
             <p>I am {props.name} and I'm {props.age}</p>
             <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.namex}/>
         </div>
     );
 };
